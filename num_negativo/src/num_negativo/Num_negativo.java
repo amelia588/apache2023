e3b519f40136b6abbfea603eb878810a7d0d19c7
amelia588 <1473967@senati.pe>
2023-03-17 10:48:37 -0500

    para ingresar los datos

---
+++ num_negativo/src/num_negativo/Num_negativo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package num_negativo;
+import java.util.*;
+/**
+ *
+ * @author Alumno
+ */
+public class Num_negativo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner name = new Scanner(System.in);
+        System.out.println("ingrese su nombre porfavor: ");
+        String nombre = name.nextLine();
+        System.out.println( "Hello" + nombre);
+    }
+    
+}

93c014336b5c1fe5c4bb151e7c9e30f348f7c95b
amelia588 <1473967@senati.pe>
2023-03-23 10:57:21 -0500

    el uso de while

---
+++ while/src/pkgwhile/While.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkgwhile;
+import java.util.Scanner;
+/**
+ *
+ * @author Alumno
+ */
+public class While {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n = 0;
+        while (n <=10)
+        {
+            System.out.println(n);
+            n++;
+        }
+    }
+    
+}

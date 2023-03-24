256a94c195fc304098a4ed8383f2be6579ee5863
amelia588 <1473967@senati.pe>
2023-03-24 10:00:37 -0500

    grafico

---
+++ grafico/src/grafico/Grafico.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package grafico;
+import java.util.Scanner;
+/**
+ *
+ * @author Alumno
+ */
+public class Grafico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("j   a   v   v     a");
+        System.out.println("j    aa  v  v  a a ");
+        System.out.println("j   aaaaa v v aaaaa");
+        System.out.println("j   a   v   v     a");
+    }
+    
+}

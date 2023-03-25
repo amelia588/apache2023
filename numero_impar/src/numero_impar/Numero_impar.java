bf7578726b49665c10a2dc22a7f0c16da0468ea9
amelia588 <1473967@senati.pe>
2023-03-25 11:00:27 -0500

    avance de numeros impares

---
+++ numero_impar/src/numero_impar/Numero_impar.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package numero_impar;
+import java.util.*;
+/**
+ *
+ * @author Alumno
+ */
+public class Numero_impar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        int i =0;
+        while(i<100){
+            System.out.println((i+1));
+            i++;
+        }
+
+
+    }
+}
+    

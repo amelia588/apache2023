3ca65a8eed9a27bd0e30eec200c84ca4c65f47fa
amelia588 <1473967@senati.pe>
2023-03-25 08:34:35 -0500

    ejercicios

---
+++ ejerciosmorning/src/ejerciosmorning/Ejerciosmorning.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerciosmorning;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Ejerciosmorning {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Morning, Morning, Morning");
+        System.out.println("         Hello a good day");
+        System.out.println("              oh!  thank you");
+        
+        System.out.println("             you are great");
+        System.out.println("         this is my pleasure");
+        System.out.println("No,...you ae welcome");
+    }
+    
+}
